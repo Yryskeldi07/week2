8e619a09b1fc0ecda4fddc1a279fbe0899d3f3d0
Yryskeldi07 <yzunusbekov241@gmail.com>
2022-06-29 09:37:59 +0600

    if else elif

---
+++ main.py
@@ -0,0 +1,247 @@
+#print(23*56/165)
+#print(1/55+2**12)
+
+#Enterword1 = input('Enter word:')
+#Enterword2 = 'False'
+#Ispalinbol1 = input('Is palindrom:')
+#Ispalinbol2 = int('True')
+#print(Enterword1 == Enterword2 and Ispalinbol1 == Ispalinbol2)
+#word = input('Enter a word:')
+#print('Is palindrom:', word ==word{::-1})
+
+#print('**********')
+#print('**********')
+#print('**********')
+#print('**********
+
+#list список колекции
+#a = [1, 2, 3, 4]
+#lists = [1, 'kdndfn', 2.3, [1,2], {'s':3}, ]
+#str_1 = 'hello world'
+#print(lists[3])
+#for i in range(10, 15, 2):
+#    print(i)
+   # print('len = ', len(lists))
+
+
+#for i in range(0, 101,2 ):
+#print(i)
+#lists_1 = list(range(0, 101,2 ))
+
+#lists_1 = list('str')
+#print(lists_1)
+
+#append()
+#lists = [1, 'nazgyl', 2.3, [1,2], {'s':3}, ]
+#len(lists)
+#lists.append({'name': 'nazgyl'})
+#lists.append([1, 3, ])
+#lists.extend([1, 3, ])
+#print(lists[1], 'before')
+#lists.insert(1, 5)
+#print(lists[1], 'after')
+#print(lists)
+
+
+
+#phone_numbers = [
+ #                '+996709372927',
+#                 '+996554928378',
+#                 '+996567898765',
+ #                '+996567898765',
+#               ]
+#black_list = phone_numbers.pop(2)
+#print(black_list)
+#print(phone_numbers)
+#phone_numbers.insert(2, black_list)
+#print(phone_numbers)
+
+#phone_numbers.remove('+996567898765')
+#print(phone_numbers.count('+996567898765'))
+#phone_numbers.reverse()
+#print(phone_numbers)
+
+#start = int(input('start '))
+#end = int(input('end '))
+#step = int(input('step '))
+#power = int(input('power '))
+
+#lists = []
+#for i in range(start, end, step,):
+ #   lists.append(i**power)
+ #   print(lists)
+#lists.clear()
+#print(lists)
+
+#empty = [1, 4, 3, 2]
+#empty.sort()
+#               print(empty)
+
+
+#print(3)
+
+
+#a = [1, 3, 4]
+#s = 'String'
+#a.reverse()
+#s.lower()
+#print(a)
+#print(s.lower())
+
+
+
+
+
+# if
+#num1 = int(input('num1: '))
+#num2 = int(input('num2: '))
+#operator = input('operator: ')
+#if operator == '+':
+  #  result = num2 + num1
+ #   print('result = {}'.format(result))
+
+
+#elif operator == '-':
+  #  result = num1 - num2
+ #   print('result = {}'.format(result))
+
+#elif operator == '*':
+    #    result = num1 * num2
+   #     print('result = {}'.format(result))
+
+
+#elif operator == '/':
+ #   result = num1 / num2
+  #  print('result = {}'.format(result))
+
+
+#if num1 > num2:
+#    print('{0} grater {1}'.format(num1, num2))
+#elif num2 > num1:
+#    print('{0} grater {1}'.format(num2, num1))
+
+
+
+
+#lists = [1, 2]
+#lists = [1, 2, 3, 4]
+
+
+
+#if 3 in lists:
+ #   print('hello')
+
+
+#or and
+
+
+#lists = [18, 22, 25, 6]
+#if 22 in lists and 25 in lists:
+#    print('yes')
+
+
+#for i in range (0, 1001) :
+ #   if i % 5 == 0:
+#print(i)
+
+#s = 0
+#for i in range(0,1000):
+ #   if i % 15 == 0:
+  #      s += i
+   #print(s)
+
+
+
+#nums = [3, 5, 15]
+#max = 1001
+
+##for num in range(1,max):
+  #  if num*i < max:
+   #     result += num*i
+#print(result)
+
+#result = []
+#for i in range(0,max):
+ #   if i%3 == 0 and i%5 == 0 and i%15 == 0:
+  #      result.append(i)
+#print(result)
+
+
+
+#temp = int(input('vedite temp: '))
+#operator = input('C or F ? : ')
+#if operator == 'C':
+#    result = 5/9*(temp-32)
+##    print('v far = {}'.format(result))
+##elif operator == 'F' :
+#    result = 9/5*(temp+32)
+#print('v cel = {}'.format(result))
+
+
+
+#s1=int(input())
+#for i in range(s1):
+#    print(i +1, end='\t')
+#    for g in range(i):
+ #print((g+1)*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
